11b93b1d6618f342fa05268fc692070b71dde463
halg830 <halg830@gmail.com>
2023-06-29 15:55:35 -0500

    actualizacion routes

--- routes/ciudad.js
+++ routes/ciudad.js
@@ -1,10 +1,6 @@
 import { Router } from "express";
 import httpCiudad from "../controllers/ciudad.js"
 import { check } from "express-validator";
-import { Router, Router } from "express";
-import httpConductor from "../controllers/conductor.js";
-import { check } from "express-validator";
-import { mongo } from "mongoose";
 
 const router = new Router();
 
@@ -14,5 +10,7 @@ router.post(
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("estado", "tiene que especificar el estado").not().isEmpty(),
   ],
-  httpCiudad.postCiudad
+  httpCiudad.postCiudades
 );
+
+export default router
