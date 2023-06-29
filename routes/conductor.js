845b007615ce0d77a8a4d9eff524183413d8d88b
Dani094 <117084576+SandyGarciaR@users.noreply.github.com>
2023-06-29 17:48:05 -0500

    correcion

--- routes/conductor.js
+++ routes/conductor.js
@@ -7,15 +7,17 @@ import { validarCampos } from "../miderwars/validar.js";
 
 const router = new Router();
 
-router.post(
-  "/hola",
+router.get("/buscar", httpConductor.getAllConductor)
+
+router.post("/hola",
   [
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("nombre", "Minimo 8 caracteres").isLength({ min: 8 }),
     check("cedula", "La cedula es obligatoria").notEmpty(),
     check("cedula", "Tiene que tener 10 digitos ").isLength({ min: 10, max: 10 }),
     validarCampos
-  ],
+  ], httpConductor.postConductor
 );
+
 
 export default router
