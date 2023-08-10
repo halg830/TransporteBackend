7b0d512d2b2d646d8473f8d264d3a9691ad91ecc
Dani094 <117084576+SandyGarciaR@users.noreply.github.com>
2023-08-10 16:52:01 -0500

    actualizacion clientes2

--- routes/cliente.js
+++ routes/cliente.js
@@ -2,8 +2,6 @@ import { Router } from "express";
 import httpCliente from "../controllers/cliente.js";
 import { check } from "express-validator";
 import { validarCampos } from "../miderwars/validar.js";
-import { mongo } from "mongoose";
-import { validarJWT } from "../miderwars/validar-jwt.js"
 
 const router = new Router();
 
@@ -11,35 +9,32 @@ router.get("/allClientes", httpCliente.getAllCliente);
 
 router.get("/cliente/:cedula", httpCliente.getClienteCedula);
 
+router.get("/cliente/:id", httpCliente.getClienteId)
+
 router.post(
   "/guardar",
   [
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("nombre", "Mínimo 8 caracteres").isLength({ min: 8 }),
     check("cedula", "La cedula es obligatoria").notEmpty(),
-    check("cedula", "Tiene que tener 10 digitos").isLength({
-      min: 10,
-      max: 10,
-    }),
+    check("cedula", "Tiene que tener 10 digitos").isLength({min: 10, max: 10,}),
+    check("email", "El correo electrónico es obligatorio").notEmpty(),
+    check("email", "Formato de correo electrónico inválido").isEmail(),
+    check("contrasena", "La contraseña es obligatoria").notEmpty(),
+    check("contrasena","La contraseña debe tener al menos 8 caracteres").isLength({ min: 8 }),
     validarCampos 
   ],
   httpCliente.postCliente
   );
 
-router.put("/editar/:cedula",
-[
-  check("nombre", "El nombre es obligatorio").not().isEmpty(),
-  check("nombre", "Mínimo 8 caracteres").isLength({ min: 8 }),
-  /* check("cedula", "La cedula es obligatoria").notEmpty(),
-  check("cedula", "Tiene que tener 10 digitos").isLength({
-    min: 10,
-    max: 10,
-  }),
-  validarCampos */
-],
-httpCliente.putCliente
-);
+router.put("/editar/:id",httpCliente.putCliente);
 
-router.delete("eliminar/:cedula", httpCliente.deleteCliente)
+router.put("/inactivar/:id", httpCliente.putClienteInactivar)
+
+router.put("/activar/:id", httpCliente.putClienteActivar)
+
+router.delete("/eliminar/:cedula", httpCliente.deleteCliente)
+
+router.delete("/eliminar/:id", httpCliente.deleteClienteId)
 
 export default router;
