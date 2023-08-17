d3ee88f9b25d1f90f6d4d6e31cbb45c1916136ae
Dani094 <117084576+SandyGarciaR@users.noreply.github.com>
2023-08-17 16:23:13 -0500

    error en ciudad

--- helpers/ciudad.js
+++ helpers/ciudad.js
@@ -3,10 +3,10 @@ import Ciudad from '../models/ciudad.js'
 const helpersCiudad = {
     existeNombre: async (nombre, req) => {
         if (nombre) {
-            const existe = await Cliente.findOne({ nombre })
+            const existe = await Ciudad.findOne({ nombre })
             if (existe) {
                 if (req.req.method === "PUT") {
-                    if (existe.nombre !== req.req.usuario.nombre)
+                    if (existe.nombre !== req.req.nombre.nombre)
                         throw new Error(`Ya existe ese serial en la base de datos!!! ${nombre}`)
 
                 } else {
