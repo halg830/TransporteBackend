16861b2fd68d77971147e17d636a89088f5abfca
Yunquil <117084576+SandyGarciaR@users.noreply.github.com>
2023-10-17 17:06:00 -0500

    populate en tiquete

--- controllers/tiquete.js
+++ controllers/tiquete.js
@@ -1,165 +1,191 @@
 import tiquete from "../models/tiquete.js";
-import Tiquete from "../models/tiquete.js"
+import Tiquete from "../models/tiquete.js";
 
 const httpTiquete = {
+  getAllTiquete: async (req, res) => {
+    try {
+      const tiquete = await Tiquete.find();
+      const tiquetePopulatePromesas = tiquete.map(async (e) => {
+        return await Tiquete.findById(e._id)
+          .populate("cliente")
+          .populate("ruta")
+          .populate("vendedor");
+      });
 
-    getAllTiquete: async (req, res) => {
-        try {
-            const tiquete = await Tiquete.find();
-            const tiquetePopulatePromesas = tiquete.map(async (e) => {
-                return await Tiquete.findById(e._id)
-                  .populate("cliente")
-                  .populate("ruta")
-                  .populate("vendedor"); 
-              });
-        
-              const tiquetePopulate = await Promise.all(tiquetePopulatePromesas);
-        
-              res.json({ tiquetePopulate });
-        } catch (error) {
-            console.log(error);
-            res.status(400).json({ error });
-        }
-    },
+      const tiquetePopulate = await Promise.all(tiquetePopulatePromesas);
 
-    getTiqueteCedula: async (req, res) => {
-        try {
-            const {cedula} = req.params
-            const tiquete = await Tiquete.find({ cedula });
-            // const tiquete = await tiquete.find({
-            //     $and:[
-            //         {cedula},
-            //         {stado:1}
-            //     ]
-            // })v
-            res.json({ tiquete });
-        } catch (error) {
-            res.status(400).json({ error });
-        }
-    },
+      res.json({ tiquetePopulate });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error });
+    }
+  },
 
-    getTiqueteId: async (req, res) => {
-        try {
-            const { id } = req.params
-            const tiquete = await Tiquete.findById(id)
-            
-            res.json({ tiquete });
-        } catch (error) {
-            res.status(400).json({ error })
+  getTiqueteCedula: async (req, res) => {
+    try {
+      const { cedula } = req.params;
+      const tiquete = await Tiquete.find({ cedula });
+      // const tiquete = await tiquete.find({
+      //     $and:[
+      //         {cedula},
+      //         {stado:1}
+      //     ]
+      // })v
+      const tiquetePopulate = await Tiquete.findById(tiquete._id)
+        .populate("ciudad_origen")
+        .populate("ciudad_destino")
+        .populate("bus");
 
-        }
-    },
+      res.json({ tiquetePopulate });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  },
 
-    getTiquetesVendidos: async(req,res)=>{
-        try {
-            const {id} = req.params
-            const vendedor = id
-            const tiquetes = await Tiquete.find({vendedor})
+  getTiqueteId: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const tiquete = await Tiquete.findById(id);
 
-            if(!tiquetes) res.json({msg: "El vendedor no ha realizado ninguna venta."})
- 
-            res.json({tiquetes})
-            
-        } catch (error) {
-            console.log(error)
-            res.status(400).json({error})
-        }
-    },
+      const tiquetePopulate = await Tiquete.findById(tiquete._id)
+        .populate("ciudad_origen")
+        .populate("ciudad_destino")
+        .populate("bus");
 
-    getFiltroFechas: async(req,res)=>{
-        try {
-            const {fechaA, fechaB} = req.params
+      res.json({ tiquetePopulate });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  },
 
-            const fechas = await Tiquete.find({
-                    $or:[
-                        {createdAt:fechaA},
-                        {createdAt:fechaB}
-                    ]
-                })
+  getTiquetesVendidos: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const vendedor = id;
+      const tiquetes = await Tiquete.find({ vendedor });
 
-            res.json(fechas)
-            
-        } catch (error) {
-            res.status(400).json({error})
-        }
-    },
+      if (!tiquetes)
+        res.json({ msg: "El vendedor no ha realizado ninguna venta." });
 
-    //POST
-    postTiquete: async (req, res) => {
-        try {
-            const { vendedor, ruta, cliente } = req.body;
-            const tiquete = new Tiquete({ vendedor, ruta, cliente });
-      
-            await tiquete.save();
+      res.json({ tiquetes });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error });
+    }
+  },
 
-            res.json({ tiquete });
-        } catch (error) {
-            res.status(400).json({ error });
-        }
-    },
+  getFiltroFechas: async (req, res) => {
+    try {
+      const { fechaA, fechaB } = req.params;
 
+      const fechas = await Tiquete.find({
+        $or: [{ createdAt: fechaA }, { createdAt: fechaB }],
+      });
 
-    //PUT
-    putTiquete: async (req, res) => {
-        try {
-            const { id } = req.params
-            const { nombre, apellido, telefono, contrasena } = req.body
+      res.json(fechas);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  },
 
-            const salt = bcryptjs.genSaltSync()
-            const newContrasena = bcryptjs.hashSync(contrasena, salt)
+  //POST
+  postTiquete: async (req, res) => {
+    try {
+      const { vendedor, ruta, cliente } = req.body;
+      const tiquete = new Tiquete({ vendedor, ruta, cliente });
 
-            const tiquete = await Tiquete.findByIdAndUpdate(id, { nombre, apellido, telefono, newContrasena }, { new: true });
-            res.json({ tiquete })
-        } catch (error) {
-            res.status(400).json({ error })
+      await tiquete.save();
 
-        }
+      const tiquetePopulate = await Tiquete.findById(tiquete._id)
+        .populate("ciudad_origen")
+        .populate("ciudad_destino")
+        .populate("bus");
 
-    },
+      res.json({ tiquetePopulate });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  },
 
-    putTiqueteInactivar: async (req, res) => {
-        try {
-            const { id } = req.params
-            const tiquete = await Tiquete.findByIdAndUpdate(id, { estado: 0 }, { new: true })
-            res.json({ tiquete })
-        } catch (error) {
-            res.status(400).json({ error })
+  //PUT
+  putTiquete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { nombre, apellido, telefono, contrasena } = req.body;
 
-        }
-    },
-    putTiqueteActivar: async (req, res) => {
-        try {
-            const { id } = req.params
-            const tiquete = await Tiquete.findByIdAndUpdate(id, { estado: 1 }, { new: true })
-            res.json({ tiquete })
-        } catch (error) {
-            res.status(400).json({ error })
+      const salt = bcryptjs.genSaltSync();
+      const newContrasena = bcryptjs.hashSync(contrasena, salt);
 
-        }
-    },
+      const tiquete = await Tiquete.findByIdAndUpdate(
+        id,
+        { nombre, apellido, telefono, newContrasena },
+        { new: true }
+      );
+      const tiquetePopulate = await Tiquete.findById(tiquete._id)
+        .populate("ciudad_origen")
+        .populate("ciudad_destino")
+        .populate("bus");
 
+      res.json({ tiquetePopulate });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  },
 
+  putTiqueteInactivar: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const tiquete = await Tiquete.findByIdAndUpdate(
+        id,
+        { estado: 0 },
+        { new: true }
+      );
+      const tiquetePopulate = await Tiquete.findById(tiquete._id)
+        .populate("ciudad_origen")
+        .populate("ciudad_destino")
+        .populate("bus");
 
-    //DELETE
-    deleteTiquete: async (req, res) => {
-        try {
-            const { cedula } = req.params
-            const tiquete = await Tiquete.findOneAndDelete({ cedula })
-            res.json({ tiquete })
-        } catch (error) {
-            res.status(400).json({ error })
-        }
+      res.json({ tiquetePopulate });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  },
+  putTiqueteActivar: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const tiquete = await Tiquete.findByIdAndUpdate(
+        id,
+        { estado: 1 },
+        { new: true }
+      );
+      const tiquetePopulate = await Tiquete.findById(tiquete._id)
+        .populate("ciudad_origen")
+        .populate("ciudad_destino")
+        .populate("bus");
 
-    },
+      res.json({ tiquetePopulate });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  },
 
-    deleteTiqueteId: async (req, res) => {
-        try {
-            const { id } = req.params
-            const tiquete = await Tiquete.findByIdAndDelete(id)
-            res.json({ tiquete })
-        } catch (error) {
+  //DELETE
+  deleteTiquete: async (req, res) => {
+    try {
+      const { cedula } = req.params;
+      const tiquete = await Tiquete.findOneAndDelete({ cedula });
+      res.json({ tiquete });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  },
 
-        }
-    },
+  deleteTiqueteId: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const tiquete = await Tiquete.findByIdAndDelete(id);
+      res.json({ tiquete });
+    } catch (error) {}
+  },
 };
 export default httpTiquete;
