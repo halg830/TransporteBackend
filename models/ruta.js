2bd6a6def4e4bbfc71bdc8317b83b307455fc251
halg830 <halg830@gmail.com>
2023-10-22 17:45:56 -0500

    cambio fecha salida de ruta a tiquete

--- models/ruta.js
+++ models/ruta.js
@@ -6,7 +6,6 @@ const Ruta = new Schema({
   ciudad_origen: { type: mongoose.Schema.Types.ObjectId, ref: "Ciudad" },
   ciudad_destino: { type: mongoose.Schema.Types.ObjectId, ref: "Ciudad" },
   hora_salida: { type: Date, required: true },
-  fecha_salida: { type: Date, required: true },
   valor: { type: Number, required: true, minlength: 4 },
   bus: { type: mongoose.Schema.Types.ObjectId, ref: "Bu" },
   createdAt: { type: Date, default: Date.now },
--- models/tiquete.js
+++ models/tiquete.js
@@ -6,6 +6,7 @@ const Tiquete = new Schema({
   vendedor: { type: mongoose.Schema.Types.ObjectId, ref: "Vendedor" },
   ruta: { type: mongoose.Schema.Types.ObjectId, ref: "Ruta" },
   cliente: { type: mongoose.Schema.Types.ObjectId, ref: "Cliente" },
+  fecha_salida: { type: Date, required: true },
   createdAt: { type: Date, default: Date.now },
   estado: { type: Number, default: 1 },
 });
