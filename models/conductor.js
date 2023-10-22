426e58eb92bb85ad05cfd5119a43209b104a179a
halg830 <halg830@gmail.com>
2023-10-22 17:34:31 -0500

    ajuste modelo conductor

--- models/conductor.js
+++ models/conductor.js
@@ -6,7 +6,7 @@ const Conductor = new Schema({
   nombre: { type: String, require: true, maxlength: 15 },
   cedula: { type: String, unique:true, require: true, minlength: 7 ,maxlength: 10 },
   createdAt: { type: Date, default: Date.now },
-  estado: { type: Boolean, default: true },
+  estado: { type: Number, default: 1 },
 });
 
 export default mongoose.model("Conductor", Conductor);
