eab66edd492068b96ce1552a45dcbbb435eee50d
halg830 <halg830@gmail.com>
2023-10-22 20:06:31 -0500

    rutas en vendedor

--- routes/vendedor.js
+++ routes/vendedor.js
@@ -42,7 +42,6 @@ router.post(
 
 router.put("/editar/:id",[
   check("nombre", "El nombre es obligatorio").notEmpty(),
-  check("nombre", "El nombre debe tener minimo 8 caracteres").isLength({min: 8}),
   check("apellido", "El apellido es obligatorio").notEmpty(),
   check("telefono", "El numero es obligatorio").notEmpty(),
   check("telefono", "El numero debe tener 10 digitos").isLength({max: 10,min: 10}),
