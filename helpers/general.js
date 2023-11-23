c1f10ca3665b797c2e3b8b754fc56369dafba727
halg830 <halg830@gmail.com>
2023-11-22 22:50:09 -0500

    e7

--- helpers/general.js
+++ helpers/general.js
@@ -1,6 +1,6 @@
 const helpersGeneral = {
     verificarEspacios: async(val, req)=>{
-        console.log(req.req.body);
+        console.log("z",req.req.body);
         if(typeof val === 'string'){
             if(val.trim()==='') throw new Error ('Solo espacios no permitidos')
         }
